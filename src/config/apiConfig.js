19618902f17e2cc2d01acb1ed5496cd223d1af33
Gagan <gd885973@gmail.com>
2024-12-02 12:31:07 +0530

    update apiconfig

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -4,7 +4,7 @@ const baseURL = "https://associatedincometax.iamdeveloper.in/api";
 
 const API = axios.create({
   baseURL: "https://associatedincometax.iamdeveloper.in/api",
-  //baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
